a2e530dd1f497e9b532b6dae6dee4ad50bab659e
“LudwingAc” <ludwing.acevedo.s@gmail.com>
2019-05-14 13:18:14 +0000

    archivo

--- energias/models.py
+++ energias/models.py
@@ -228,6 +228,9 @@ class infografia(models.Model):
 
     titulo = models.CharField(max_length=60)
     imagen = models.ImageField(upload_to='infografia', blank=True)
+    upload = models.FileField(upload_to='gas', blank=True)
+    nombre_boton1 = models.CharField(max_length=80)
+    nombre_boton2 = models.CharField(max_length=80)
     fecha_creacion = models.DateTimeField(default=datetime.now, blank=True)
     disponible = models.BooleanField()
 
